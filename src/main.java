eda4ec733e7e7d0f89a44d3faee269dfb76aec7c
Foad <FuadHr>
2021-12-11 10:07:20 +0330

    first

---
+++ src/main.java
@@ -0,0 +1,23 @@
+public class main {
+    public static void main(String[] args) {
+        try {
+            //myMethod();
+        }catch (Exception err){
+            err.printStackTrace();
+        }
+
+    }
+    /*public static void myMethod() throws Exception{
+        try {
+            throw new Exception("E1");
+        }catch (Exception err){
+            throw new Exception("E2");
+        }finally {
+            //throw new Exception("E3");
+            System.out.print("E3");
+        }
+
+
+    }*/
+
+}
