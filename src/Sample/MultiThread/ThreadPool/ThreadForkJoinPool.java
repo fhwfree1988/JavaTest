c7421b44ac74554abc6b74b8a98a7558ff0a0ab5
Foad <FuadHr>
2021-12-27 16:35:18 +0330

    threadpool

---
+++ src/Sample/MultiThread/ThreadPool/ThreadForkJoinPool.java
@@ -0,0 +1,47 @@
+package Sample.MultiThread.ThreadPool;
+
+import org.assertj.core.util.Sets;
+
+import java.util.Set;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.ForkJoinTask;
+import java.util.concurrent.RecursiveTask;
+import java.util.stream.Collectors;
+
+public class ThreadForkJoinPool {
+    //https://www.baeldung.com/thread-pool-java-and-guava
+    public static void main(String[] args) {
+        TreeNode tree = new TreeNode(5,
+                new TreeNode(3), new TreeNode(2,
+                new TreeNode(2), new TreeNode(8)));
+
+        ForkJoinPool forkJoinPool = ForkJoinPool.commonPool();
+        int sum = forkJoinPool.invoke(new CountingTask(tree));
+    }
+    static class TreeNode {
+
+        int value;
+
+        Set<TreeNode> children;
+
+        TreeNode(int value, TreeNode... children) {
+            this.value = value;
+            this.children = Sets.newHashSet(children);
+        }
+    }
+    public static class CountingTask extends RecursiveTask<Integer> {
+
+        private final TreeNode node;
+
+        public CountingTask(TreeNode node) {
+            this.node = node;
+        }
+
+        @Override
+        protected Integer compute() {
+            return node.value + node.children.stream()
+                    .map(childNode -> new CountingTask(childNode).fork())
+                    .collect(Collectors.summingInt(ForkJoinTask::join));
+        }
+    }
+}
