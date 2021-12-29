bbf26f8e5dbe00f321c8c4d0e52c2f646c05643f
Foad <FuadHr>
2021-12-29 17:03:44 +0330

    change

--- src/ir/Me/session_1/e01/StringTest.java
+++ src/ir/Me/session_1/e01/StringTest.java
@@ -9,9 +9,10 @@ public class StringTest {
         String s1 = "abc";
         String s2 = "abc";
 
-//        String s1 = new String("abc");
-//        String s2 = new String("abc");
+        String s3 = new String("abc");
+        String s4 = new String("abc");
 
         logger.info(s1 == s2);
+        logger.info(s3 == s4);
     }
 }
