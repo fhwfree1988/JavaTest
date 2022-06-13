5bff773df4d6d396a124724f55c1232eb35bd644
Foad <FuadHr>
2022-06-13 15:38:26 +0430

    change

--- src/Sample/Stream/StreamSampleOne.java
+++ src/Sample/Stream/StreamSampleOne.java
@@ -16,14 +16,15 @@ class employeeRepository{
     }
 }
 public class StreamSampleOne {
-//https://stackify.com/streams-guide-java-8/
-    public static Employee[] arrayOfEmps ={
+    //https://stackify.com/streams-guide-java-8/
+    public static Employee[] arrayOfEmps = {
             new Employee(1, "Jeff Bezos", 100000.0),
-            new Employee(2,"Bill Gates", 200000.0),
-            new Employee(3,"Mark Zuckerberg", 300000.0)
+            new Employee(2, "Bill Gates", 200000.0),
+            new Employee(3, "Mark Zuckerberg", 300000.0)
     };
 
     private static List<Employee> empList = Arrays.asList(arrayOfEmps);
+
     public static void main(String[] args) {
         Stream.of(arrayOfEmps);
 
@@ -46,7 +47,7 @@ public class StreamSampleOne {
 
     @Test
     public static void whenMapIdToEmployees_thenGetEmployeeStream() {
-        Integer[] empIds = { 1, 2, 3 };
+        Integer[] empIds = {1, 2, 3};
 
         List<Employee> employees = Stream.of(empIds)
                 .map(employeeRepository::findById)
@@ -54,3 +55,4 @@ public class StreamSampleOne {
 
         assertEquals(employees.size(), empIds.length);
     }
+}
