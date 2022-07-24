0e5e2622a5b8549c86cfbf94be33a67e797379d3
Foad <FuadHr>
2022-07-24 15:38:27 +0430

    change

--- src/main.java
+++ src/main.java
@@ -2,10 +2,21 @@ public class main {
     public static void main(String[] args) {
         try {
             //myMethod();
+            //a(5);
+
+
+
         }catch (Exception err){
             err.printStackTrace();
         }
 
+    }
+    static void a(int a , int...b){
+        System.out.print(a);
+    }
+
+    static void a(int...b){
+        System.out.print(b);
     }
     /*public static void myMethod() throws Exception{
         try {
