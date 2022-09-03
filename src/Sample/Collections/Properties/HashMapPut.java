8524fc86424524767e1dd32907b3ec857ae72fe4
Foad <FuadHr>
2022-09-03 18:27:54 +0430

    change

---
+++ src/Sample/Collections/Properties/HashMapPut.java
@@ -0,0 +1,18 @@
+package Sample.Collections.Properties;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashMapPut {
+    public static void main(String[] args) {
+        Map m1 = new HashMap();
+        m1.put("Zara", "8");
+        m1.put("Mahnaz", "31");
+        m1.put("Ayan", "12");
+        m1.put("Zara", "20");
+
+        System.out.println();
+        System.out.println(" Map Elements");
+        System.out.print("\t" + m1);
+    }
+}
---
+++ src/Sample/OOP/Keyword.java
@@ -0,0 +1,12 @@
+package Sample.OOP;
+
+public class Keyword {
+    /*static*/public Keyword() {
+        System.out.println("Keyword");
+    }
+
+    public static void main(String[] args) {
+        Keyword keyword = new Keyword();
+
+    }
+}
