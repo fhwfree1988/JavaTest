c112893d55dcce9a70a501eb298e57a26c619329
Foad <f.h.w.free1988@gmail.com>
2023-03-27 11:59:57 +0430

    change

---
+++ src/Sample/sampleTest/FinalInteger.java
@@ -0,0 +1,15 @@
+package Sample.sampleTest;
+
+public class FinalInteger {
+
+
+    public static void main(String[] args) {
+        final int num1 = 10, num2 = 20;
+
+        while(num1 < num2)
+        {
+            System.out.println(num1);
+        }
+    }
+
+}
