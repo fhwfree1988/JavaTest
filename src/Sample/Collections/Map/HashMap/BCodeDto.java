d2b133f936d79e11217c3b13a09990062ad91cf4
Foad <f.h.w.free1988@gmail.com>
2023-06-18 16:42:04 +0330

    change

---
+++ src/Sample/Collections/Map/HashMap/BCodeDto.java
@@ -0,0 +1,82 @@
+package Sample.Collections.Map.HashMap;
+
+public class BCodeDto {
+	private String serialCode;//12 Digit
+	private String statusCode;//1 Digit
+	private String moeinCode;//2 Digit
+	private String budgetIncomeCode;//6 Digit
+	private String provinceCode;//2 Digit
+	private String organizationCode;//4 Digit
+	private String idCharacteristicCode;//1 Digit
+	private String Cost;//12 Digit
+
+	/*private String value1;//1 Digit
+	private String value2;//1 Digit
+	private String value3;//1 Digit*/
+
+
+	public String getSerialCode() {
+		return serialCode;
+	}
+
+	public void setSerialCode(String serialCode) {
+		this.serialCode = serialCode;
+	}
+
+	public String getStatusCode() {
+		return statusCode;
+	}
+
+	public void setStatusCode(String statusCode) {
+		this.statusCode = statusCode;
+	}
+
+	public String getMoeinCode() {
+		return moeinCode;
+	}
+
+	public void setMoeinCode(String moeinCode) {
+		this.moeinCode = moeinCode;
+	}
+
+	public String getBudgetIncomeCode() {
+		return budgetIncomeCode;
+	}
+
+	public void setBudgetIncomeCode(String budgetIncomeCode) {
+		this.budgetIncomeCode = budgetIncomeCode;
+	}
+
+	public String getProvinceCode() {
+		return provinceCode;
+	}
+
+	public void setProvinceCode(String provinceCode) {
+		this.provinceCode = provinceCode;
+	}
+
+	public String getOrganizationCode() {
+		return organizationCode;
+	}
+
+	public void setOrganizationCode(String organizationCode) {
+		this.organizationCode = organizationCode;
+	}
+
+	public String getIdCharacteristicCode() {
+		return idCharacteristicCode;
+	}
+
+	public void setIdCharacteristicCode(String idCharacteristicCode) {
+		this.idCharacteristicCode = idCharacteristicCode;
+	}
+
+	public String getCost() {
+		return Cost;
+	}
+
+	public void setCost(String cost) {
+		Cost = cost;
+	}
+
+}
--- src/Sample/Collections/Map/HashMap/HashMapEntrySetExample.java
+++ src/Sample/Collections/Map/HashMap/HashMapEntrySetExample.java
@@ -4,6 +4,7 @@ package Sample.Collections.Map.HashMap;
 import java.util.*;
 import java.util.Map.Entry;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class HashMapEntrySetExample {
 
@@ -41,6 +42,117 @@ public class HashMapEntrySetExample {
         List<String> fruits = Arrays.asList("apple","mango");
         System.out.println(fruits.stream().map(fruit -> fruit.split("")).flatMap(Arrays::stream).collect(Collectors.toSet()));
 
+        List<String> lst = StringToList();
+
+
+        BCodeDto bCodeDto = new BCodeDto();
+       /* bCodeDto.setSerialCode("012345678901");
+        bCodeDto.setStatusCode("1");
+        bCodeDto.setMoeinCode("01");
+        bCodeDto.setBudgetIncomeCode("150101");
+        bCodeDto.setProvinceCode("82");
+        bCodeDto.setOrganizationCode("0115");
+        bCodeDto.setIdCharacteristicCode("2");
+        bCodeDto.setCost("000999910000000");*/
+
+        bCodeDto.setSerialCode("111111111111");
+        bCodeDto.setStatusCode("2");
+        bCodeDto.setMoeinCode("01");
+        bCodeDto.setBudgetIncomeCode("150101");
+        bCodeDto.setProvinceCode("74");
+        bCodeDto.setOrganizationCode("0115");
+        bCodeDto.setIdCharacteristicCode("2");
+        bCodeDto.setCost("000000000245551");
+
+        /*bCodeDto.setSerialCode("111111111112");
+        bCodeDto.setStatusCode("2");
+        bCodeDto.setMoeinCode("01");
+        bCodeDto.setBudgetIncomeCode("150101");
+        bCodeDto.setProvinceCode("74");
+        bCodeDto.setOrganizationCode("0115");
+        bCodeDto.setIdCharacteristicCode("2");
+        bCodeDto.setCost("000000000362543");*/
+
+        /*bCodeDto.setSerialCode("111111111115");
+        bCodeDto.setStatusCode("2");
+        bCodeDto.setMoeinCode("01");
+        bCodeDto.setBudgetIncomeCode("150101");
+        bCodeDto.setProvinceCode("74");
+        bCodeDto.setOrganizationCode("0115");
+        bCodeDto.setIdCharacteristicCode("2");
+        bCodeDto.setCost("000000000101001");*/
+
+       /* bCodeDto.setSerialCode("111111111118");
+        bCodeDto.setStatusCode("2");
+        bCodeDto.setMoeinCode("01");
+        bCodeDto.setBudgetIncomeCode("150101");
+        bCodeDto.setProvinceCode("74");
+        bCodeDto.setOrganizationCode("0115");
+        bCodeDto.setIdCharacteristicCode("2");
+        bCodeDto.setCost("000000000060103");*/
+
+        String bCode = generateCodeString(bCodeDto);
+        bCode.toString();
     }
 
+    private static List<String> StringToList(){
+        List<String> serialCode = new ArrayList<>(13);
+        String val = "012345678910";
+        serialCode = Stream.of(val.split("")).collect(Collectors.toList());
+        StringBuilder stringBuilder = new StringBuilder(12);
+        stringBuilder.append("12345678910");
+        return serialCode;
+    }
+
+
+    /*----------------BehdadCode------------------------*/
+    public static String generateCodeString(BCodeDto bCodeDto){
+        List<String> CodeArray = generateCodeArray(bCodeDto);
+        String code = String.join("", CodeArray);
+        String X = Verhoeff.generateVerhoeff(code);
+
+        List<String> CodeArrayReversed = generateCodeArrayReversed(bCodeDto);
+        String codeReversed = String.join("", CodeArrayReversed);
+        String Y = Verhoeff.generateVerhoeff(codeReversed);
+
+        CodeArray.add(1,X);
+        CodeArray.add(2,Y);
+        String behCode = String.join("", CodeArray);
+        return behCode;
+    }
+    public static List<String> generateCodeArray(BCodeDto bCodeDto){
+        List<String> CodeArray = GenerateCode(bCodeDto);
+        CodeArray.addAll(Stream.of(bCodeDto.getCost().split("")).collect(Collectors.toList()));
+        return CodeArray;
+    }
+
+    private static List<String> generateCodeArrayReversed(BCodeDto bCodeDto){
+        List<String> CodeArrayReversed = GenerateCode(bCodeDto);
+        Collections.reverse(CodeArrayReversed);
+
+        List<String> costReversed = Stream.of(bCodeDto.getCost().split("")).collect(Collectors.toList());
+        Collections.reverse(costReversed);
+        CodeArrayReversed.addAll(costReversed);
+        return CodeArrayReversed;
+    }
+
+    private static List<String> GenerateCode(BCodeDto bCodeDto){
+        List<String> behdadCodeArray = new ArrayList<>(50);
+
+        behdadCodeArray.addAll(Stream.of(bCodeDto.getIdCharacteristicCode().split("")).collect(Collectors.toList()));
+        behdadCodeArray.addAll(Stream.of(bCodeDto.getOrganizationCode().split("")).collect(Collectors.toList()));
+        behdadCodeArray.addAll(Stream.of(bCodeDto.getProvinceCode().split("")).collect(Collectors.toList()));
+        behdadCodeArray.addAll(Stream.of(bCodeDto.getBudgetIncomeCode().split("")).collect(Collectors.toList()));
+        behdadCodeArray.addAll(Stream.of(bCodeDto.getMoeinCode().split("")).collect(Collectors.toList()));
+        behdadCodeArray.addAll(Stream.of(bCodeDto.getStatusCode().split("")).collect(Collectors.toList()));
+        behdadCodeArray.addAll(Stream.of(bCodeDto.getSerialCode().split("")).collect(Collectors.toList()));
+
+        return behdadCodeArray;
+    }
+
+   /* private static String generateSerialCode(BCodeDto bCodeDto){
+        List<String> serialCode = new ArrayList<>(13);
+        String val = "12345678910";
+        return val;
+    }*/
 }
